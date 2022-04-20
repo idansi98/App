77dac06c8c0bbd9fdc006ab9a9526e3dc9296c45
idansi98 <idansi98@gmail.com>
2022-04-20 14:24:41 +0300

    a

--- src/chatStuff/aboveContactsRow.js
+++ src/chatStuff/aboveContactsRow.js
@@ -1,24 +1,30 @@
 import AddContactButton from "./addContactButton";
 import Example from "./example";
 
-function AboveContactsRow({setCurrentChat, isSmall}) {
-  if(isSmall!=true) {
+function AboveContactsRow({ setCurrentChat, isSmall }) {
+  if (isSmall != true) {
     return (
       <div className="col-sm-4 border-right border-secondary">
-      <img
-        src={global.currentUser.picture}
-        alt=""
-        className="profile-image rounded-circle"
-      />
-      <Example />
-      <span className="float-right mt-2" id = "AboveSpan">
-        <span className="input-group mb-3">
-          <AddContactButton setCurrentChat={setCurrentChat} />
+        <img
+          src={global.currentUser.picture}
+          alt=""
+          className="profile-image rounded-circle"
+        />
+        <Example />
+        <span className="float-right mt-2" >
+          <span className="input-group mb-3">
+          <span id ="Hey">
+              <input type="text" className="form-control" placeholder="Displayname"
+               aria-label="Username" aria-describedby="basic-addon1" id="DisplayNameInput"/>
+               </span>
+               <span>
+            <AddContactButton setCurrentChat={setCurrentChat} />
+            </span>
+          </span>
         </span>
-      </span>
-    </div>
-  )
-  } else{
+      </div>
+    )
+  } else {
     return <></>
   }
 
