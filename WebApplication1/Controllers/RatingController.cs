9005406432eacac24c70ac2c174dfa9cfe9763f2
River <tziony.i@gmail.com>
2022-05-10 01:37:17 +0300

    /Rating /Rate share _Layout

---
+++ WebApplication1/Controllers/RatingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class RatingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
