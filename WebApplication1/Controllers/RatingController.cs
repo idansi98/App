0a975b984cd3a883056575117895c0b2bba4d92d
River <tziony.i@gmail.com>
2022-05-10 02:21:31 +0300

    started rating

--- WebApplication1/Controllers/RatingController.cs
+++ WebApplication1/Controllers/RatingController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -6,7 +7,11 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<Rating> ratings = new List<Rating>();
+            ratings.Add(new Rating { DateTime = 1, ID = 2, ReviewerName = "Ido", ReviewText = "Best app ever!" , Score = 5});
+            ratings.Add(new Rating { DateTime = 2, ID = 3, ReviewerName = "Idan", ReviewText = "Marvelous!", Score = 1 });
+            ViewBag.Rating = ratings;
+            return View(ratings);
         }
     }
 }
---
+++ WebApplication1/Models/Rating.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Rating
+    {
+        public int ID { get; set; }
+        public string ReviewerName { get; set; }
+        public int Score { get; set; }
+        public string ReviewText { get; set; }
+        public int DateTime { get; set; }
+    }
+}
