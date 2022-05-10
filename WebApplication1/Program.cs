89dcc22fc1ea07e1b751c38d17b9c60908f5e590
River <tziony.i@gmail.com>
2022-05-10 16:20:02 +0300

    Added DB and base pages fr rating

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<WebApplication1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApplication1Context") ?? throw new InvalidOperationException("Connection string 'WebApplication1Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
